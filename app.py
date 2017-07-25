2376b00dbad8017008c77bd40fa50382f71d059c
Dhiraj Borade <dhirajborade@gmail.com>
2017-07-25 15:39:34 -0400

    Resolved error related to db

--- app.py
+++ app.py
@@ -13,12 +13,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'MY_SECRET_KEY'
 api = Api(app)
 
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
 jwt = JWT(app, authenticate, identity)  # /auth
 
 # class Student(Resource):
@@ -35,6 +29,5 @@ api.add_resource(UserRegister, '/register')
 
 if __name__ == "__main__":
     from db import db
-
     db.init_app(app)
     app.run(host='127.0.0.1', port=5001, debug=True)
